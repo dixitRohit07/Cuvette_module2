c5ab77431413427e6fbeae5110b546eef8901b0a
dixitRohit07 <158456266+dixitRohit07@users.noreply.github.com>
2024-10-09 22:47:17 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import FrontPage from './components/FrontPag';
+// import FrontPage from "../components/FrontPage"
+
+
+function App() {
+  return (
+    <>
+      <div style={{ display: 'flex' }}>
+      <FrontPage/>
+      </div>
+    </>
+  );
+}
+
+export default App;
